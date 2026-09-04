d1a3889cdcbc4ee88b4208db079b1b8f9019fd3c
prosnero-glitch <prosnero@gmail.com>
2026-09-04 15:14:26 +0100

    hover on nav link

--- Pathway.js
+++ Pathway.js
@@ -2,3 +2,6 @@ let logoContainer = document.querySelector('.logo-container');
 let logoContainerHeight = logoContainer.offsetHeight;
 const logo = document.querySelector('.logo');
 let logoHeight = logo.offsetHeight;
+let navlist = document.querySelectorAll('.nav-list li');(
+    
+)
